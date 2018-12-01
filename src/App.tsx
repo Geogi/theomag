1edd1ba1406b492465e2a40b583f9824e82e361a
Paul Blouët <1818316+Geogi@users.noreply.github.com>
2018-12-01 15:09:45 +0100

    The map now works

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import 'leaflet-defaulticon-compatibility';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
+import 'leaflet/dist/leaflet.css';
 import * as React from 'react';
-import './App.css';
+import './App.css'
 
 import {Map as LeafLetMap, Marker, Popup, TileLayer} from "react-leaflet";
 
 const position: [number, number] = [48.755517, 2.281012];
 const App = () => (
-    <LeafLetMap center={position} zoom={13}>
+    <LeafLetMap center={position} zoom={17}>
         <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
