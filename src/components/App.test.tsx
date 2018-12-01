0ed6e1a2ef7a22d183476e592303ebb1a2ded2da
Paul Blouët <1818316+Geogi@users.noreply.github.com>
2018-12-01 17:19:56 +0100

    broke the tests

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import {devToolsEnhancer} from "redux-devtools-extension";
+import mapState from "../reducers";
 import App from './App';
+
+const store = createStore(
+    mapState,
+    devToolsEnhancer({})
+);
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App/>, div);
+    ReactDOM.render(
+        <Provider store={store}>
+            <App/>
+        </Provider>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
