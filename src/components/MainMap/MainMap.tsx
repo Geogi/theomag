78bf426bd179676a0a91a412c211eb3d11b8bce9
Paul Blouët <1818316+Geogi@users.noreply.github.com>
2018-12-03 16:32:55 +0100

    add local persistence with redux-persistence and localforage
    + still no server (including persistence there)
    
    + compression for the state seems necessary, but the
      TypeScript definitions are buggy for the compression
      lib; forked and PRed, but using self-fork until PR is
      merged.
    
    + because it would mean too many nodes with no options to
      remove them, exactly that has been done (that means
      integration with Material UI for React)

--- src/components/MainMap/MainMap.tsx
+++ src/components/MainMap/MainMap.tsx
@@ -1,19 +1,27 @@
+import Button from "@material-ui/core/Button/Button";
+import {Delete} from "@material-ui/icons";
 import 'leaflet-defaulticon-compatibility';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
 import 'leaflet/dist/leaflet.css';
 import * as React from 'react';
-import {Map as LeafLetMap, Marker, TileLayer} from "react-leaflet";
+import {Map as LeafLetMap, Marker, Popup, TileLayer} from "react-leaflet";
 import {IPto} from "../../reducers";
 import './MainMap.css';
 
-const MainMap = ({lat, lng, zoom, ptoAdd, pto}: any) => (
+const MainMap = ({lat, lng, zoom, ptoAdd, ptoDel, pto}: any) => (
     <LeafLetMap center={[lat, lng]} zoom={zoom} ondblclick={ptoAdd} doubleClickZoom={false}>
         <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
         />
         {pto.map((pos: IPto) => (
-            <Marker key={pos.key} position={[pos.y, pos.x]}/>
+            <Marker key={pos.key} position={[pos.y, pos.x]}>
+                <Popup>
+                    <Button variant="flat" color="primary">
+                        <Delete onClick={ptoDel(pos.key)}/>
+                    </Button>
+                </Popup>
+            </Marker>
         ))}
     </LeafLetMap>
 );
